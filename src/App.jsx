65dafad1cd57ea44813aacc295f79cbb1d4ce84c
Virajb19 <virajb853@gmail.com>
2024-09-16 19:12:22 +0530

    added lenis for smooth scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react"
 import Navbar from "./components/Navbar"
-import LocomotiveScroll from 'locomotive-scroll'
 import 'locomotive-scroll/dist/locomotive-scroll.css'
 import HeroSection from "./components/HeroSection"
 import FeatureSection from "./components/FeatureSection"
@@ -8,16 +7,27 @@ import WorkFlow from "./components/WorkFlow"
 import Pricing from "./components/Pricing"
 import Testimonials from "./components/Testimonials"
 import Footer from "./components/Footer"
+import Lenis from "lenis"
 
 export default function App() {
 
-    // useEffect(() => {
-    //     const scroll = new LocomotiveScroll({
-    //         el: document.querySelector('[data-scroll-container]'),
-    //         smooth: true,
-    //       })   
-    //       return () => scroll.destroy() 
-    // }, [])
+      useEffect(() => {
+            const lenis = new Lenis({
+                  duration: 1.5,
+                  easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+                  smooth: true,
+                  infinite: false,
+                })
+
+                function raf(time) {
+                  lenis.raf(time);
+                  requestAnimationFrame(raf)
+                }
+            
+                requestAnimationFrame(raf)
+            
+                return () => lenis.destroy()
+      })
 
     return <main className="w-full min-h-screen bg-[#111111]">
             <Navbar />
